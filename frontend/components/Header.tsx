465e390135f3001cbb7db7fe7cc66757d8015762
ZagZx <azevedodvictor@gmail.com>
2025-12-04 00:14:08 -0300

    refactor: componentes movidos para fora de app

---
+++ frontend/components/Header.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import Button from "./ui/Button";
+
+export default function Header() {
+  return (
+    <header className="flex flex-row justify-center gap-1">
+      <Link href="/">
+        <Button content="Início" />
+      </Link>
+      <Link href="/login">
+        <Button content="Login" />
+      </Link>
+      <Link href="/cadastro">
+        <Button content="Cadastro" />
+      </Link>
+      <Link href="/logout">
+        <Button content="Sair" />
+      </Link>
+    </header>
+  );
+}
