609815a2e2562461de7d82dad2989e76b43e46ca
Pedro Victor <azevedodvictor@gmail.com>
2025-12-04 17:50:10 -0300

    atualizar contestação (com problemas)

--- backend/controllers/contestacoes_controller.py
+++ backend/controllers/contestacoes_controller.py
@@ -12,7 +12,42 @@ from backend.utils import criar_e_obter_diretorio_contestacao, salvar_imagem
 
 contestacoes_bp = Blueprint('contestacoes', __name__)
 
+@contestacoes_bp.route('/contestacao/<int:contestacao_id>/atualizar', methods=['POST'])
+@login_required
+def atualizar_contestacao(contestacao_id):
+    contestacao: Contestacao = Contestacao.query.get_or_404(contestacao_id)
 
+    dados = request.form
+
+    if current_user.get_id() != contestacao.usuario_id:
+        return jsonify({"message": "Apenas o autor da contestação pode atualiza-la"}), 401
+    
+    arquivos = request.files
+
+    motivo = dados.get("motivo")
+    if motivo and motivo != contestacao.motivo:
+        contestacao.motivo = motivo
+    
+    imagens = arquivos.getlist("fotos")
+    # print(imagens)
+    if imagens: # objeto com imagem vazia, por algum motivo desconhecido
+        if len(contestacao.provas) + len(imagens) >= 5:
+            return jsonify({"message": "O limite de 5 imagens foi atingido"}), 400
+
+        path = criar_e_obter_diretorio_contestacao(contestacao.id)
+        try:
+            for img in imagens:
+                filename = salvar_imagem(path, img)
+                url = f"/api/uploads/contestacoes/{contestacao.id}/{filename}"
+                prova_contestacao = ProvaContestacao(url=url, nome_arquivo=filename, contestacao=contestacao)
+                db.session.add(prova_contestacao)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            # apagar as imagens do upload
+            return jsonify({"message": f"Erro ao adicionar as fotos de prova de constestação: {e}"}), 500
+
+    return jsonify({"message": "Contestação atualizada com sucesso", "contestacao": contestacao.to_dict()}), 200
 # falta testar
 @contestacoes_bp.route('/reclamacao/<int:reclamacao_id>/contestar', methods=['POST'])
 @login_required
@@ -53,7 +88,7 @@ def contestar_reclamacao(reclamacao_id):
     try:
         for img in imagens:
             filename = salvar_imagem(path, img)
-            url = f"/api/uploads/contestacoes/{reclamacao.id}/{filename}"
+            url = f"/api/uploads/contestacoes/{contestacao.id}/{filename}"
             prova_contestacao = ProvaContestacao(url=url, nome_arquivo=filename, contestacao=contestacao)
             db.session.add(prova_contestacao)
         db.session.commit()
