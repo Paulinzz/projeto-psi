a74c8f9d339f483fda55baf5f1436b22e8278e9c
ZagZx <azevedodvictor@gmail.com>
2025-12-07 00:26:20 -0300

    feat: redirecionamento para a página de login quando tenta acessar página que requer autenticação

---
+++ frontend/proxy.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
+
+export async function proxy(request: NextRequest) {
+  const apiUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+  
+  const response = await fetch(`${apiUrl}/api/me`, {
+    method: "GET",
+    headers: {
+      cookie: request.headers.get("cookie") ?? ""
+    }
+  }); 
+
+  const usuario = await response.json();
+
+  if (!usuario.autenticado) {
+    return NextResponse.redirect(new URL(`/login?redirect=${request.nextUrl.pathname}`, request.url));
+  }
+
+  return NextResponse.next();
+}
+
+// SÓ EXECUTA O PROXY EM ROTAS QUE REQUEREM AUTENTICAÇÃO
+export const config = {
+  matcher: [
+    "/usuario/:path*",
+    "/logout",
+  ]
+};
